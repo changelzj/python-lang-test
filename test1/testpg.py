9664988b93a0f71b9939fbcee52020c51c88f3f6
liuzijian <t-zijian.liu@pcitc.com>
2025-12-25 14:17:42 +0800

    add

---
+++ test1/testpg.py
@@ -0,0 +1,38 @@
+
+# from trade import pay
+# from trade import order
+#
+# print(pay.timeout)
+# pay.wechat_pay()
+#
+# print(order.max_amount)
+# order.create_order()
+
+##################
+
+# from trade import pay as p
+# from trade import order as o
+#
+# print(p.timeout)
+# p.wechat_pay()
+#
+# print(o.max_amount)
+# o.create_order()
+
+################
+#
+# from trade import *
+#
+# print(a)
+# print(b)
+
+#print(order.max_amount)
+#pay.wechat_pay()
+
+########################
+
+import trade
+
+trade.order.create_order()
+print(trade.a)
+
---
+++ test1/trade/__init__.py
@@ -0,0 +1,11 @@
+
+print('trade init')
+
+a = 100
+b = 200
+
+ import order
+ import pay
+
+#__all__ = ['order', 'a', 'b']
+
